eb2a599fc70ea3f286a96207ef2bb5ddc64d2af6
tak-narae <dddddum@naver.com>
2025-02-13 23:12:12 +0900

    cloudtype 테스트

--- src/pages/member/Join.jsx
+++ src/pages/member/Join.jsx
@@ -45,6 +45,7 @@ const Join = () => {
     //##### 아이디
     const response = await axios.get(`${API_URL}/users/check-id`, {
       params:{user_id:formData.user_id},
+      withCredentials: true //CORS 문제 방지
     });
     try {
       console.log("값체크", formData.user_id);
